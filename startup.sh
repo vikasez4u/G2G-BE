f4592ce2fd6874b2377db16fd595b33bbee8128d
vikasez4u <vikasez4u@gmail.com>
2025-07-04 22:39:03 +0530

    increasing timeout

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Start your app (adjust this line as needed)
-uvicorn main:app --host 0.0.0.0 --port 8000
+#uvicorn main:app --host 0.0.0.0 --port 8000
+
+gunicorn main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 600
